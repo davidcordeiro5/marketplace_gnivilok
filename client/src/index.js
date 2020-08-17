f5a09e72d067dda6d13fb68adcc5bd9872128c6f
David Cordeiro <david.cordeiro@epitech.eu>
2020-08-17 18:25:31 +0200

    add ApolloProvider and dependencies

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,9 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ApolloProvider from './ApolloProvider'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render(ApolloProvider,
   document.getElementById('root')
 );
 
