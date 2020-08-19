77f30f282e16adcb2b59f2c34e17d464cd48a6c2
David Cordeiro <david.cordeiro@epitech.eu>
2020-08-19 16:57:53 +0200

    login ok

--- client/src/page/Login/Login.jsx
+++ client/src/page/Login/Login.jsx
@@ -1,40 +1,87 @@
-import React from 'react';
+import React, { useState } from 'react';
+import gql from 'graphql-tag';
+import { useMutation } from '@apollo/react-hooks';
 import { useForm } from 'react-hook-form';
-import { H2 } from '@bootstrap-styled/v4'
-import { 
+import { H2 } from '@bootstrap-styled/v4';
+import {
   PageWrapper,
   FromWrapper,
-  From, 
-  FromGroup, 
+  From,
+  FromGroup,
   InputSubmit,
   InputText,
-  Label } from '../../reusable/resable'
+  Label
+} from '../../reusable/resable'
 
+const LOGIN_USER = gql`
+ mutation login(
+    $username: String!
+    $password: String!
+  ) {
+    login(
+        username: $username,
+        password: $password
+    ) {
+      id
+      username
+      userType
+      token
+    }
+  }
+`;
 
-const Login = () => {
+const Login = (props) => {
+  const { handleSubmit, register, errors } = useForm();
+  const [graphqlErrorsLogin, setGraphqlErrorsLogin] = useState({})
+  const [loginInput, setLoginInput] = useState({
+    username: '',
+    password: ''
+  })
+
   const theme = {
     '$font-size-h2': '2rem',
     '$headings-font-weight': 'bold',
     '$headings-color': '#313131'
   }
-  const { handleSubmit, register, errors } = useForm();
-  const onSubmit = values => console.log(values);
 
+  const [loginUser, { loading }] = useMutation(LOGIN_USER, {
+    update(proxy, result) {
+      props.history.push('/')
+    },
+    onError(err) {
+      console.log(err.graphQLErrors[0].extensions.errors)
+      setGraphqlErrorsLogin(err.graphQLErrors[0].extensions.errors)
+    },
+    variables: loginInput
+  })
+  
+  const onChange = (event) => {
+    setLoginInput({ ...loginInput, [event.target.name]: event.target.value })
+  }
+
+  const onSubmit = (event) => {
+    loginUser();
+    console.log('event', event, loginInput)
+  };
   return (
     <PageWrapper>
       <H2 style={{ marginBottom: 40 }} theme={theme}>Login</H2>
       <FromWrapper>
+        {(!loading) ? 
         <From onSubmit={handleSubmit(onSubmit)}>
           <FromGroup>
-            <Label for="name">Name :</Label>
-            <InputText id="name" name="name" ref={register} />
+            <Label htmlFor="username">Name :</Label>
+            <InputText id="username" name="username" onChange={onChange} ref={register({ required: true })} />
+            {errors.username && <span>This field is required</span>}
           </FromGroup>
           <FromGroup>
-            <Label for="password">Password :</Label>
-            <InputText id="password" name="password" type="password" ref={register} />
+            <Label htmlFor="password">Password :</Label>
+            <InputText id="password" name="password" type="password" onChange={onChange} ref={register({ required: true })}/>
+            {errors.password && <span>This field is required</span>}
           </FromGroup>
           <InputSubmit type="submit" value="Connection" />
-        </From>
+          {Object.keys(graphqlErrorsLogin).length > 0 ? <p>graphqlErrorsLogin: Bad name or password</p> : null}
+        </From>: (<p>loading...</p>)}
       </FromWrapper>
 
     </PageWrapper>
