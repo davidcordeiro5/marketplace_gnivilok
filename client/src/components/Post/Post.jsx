5a03f0ced62bd49f3a4182f3b1963b7ba4dafcc0
David Cordeiro <david.cordeiro@epitech.eu>
2020-08-20 14:43:11 +0200

    update Post component without mock values

--- client/src/components/Post/Post.jsx
+++ client/src/components/Post/Post.jsx
@@ -7,15 +7,15 @@ import {
   CardSubtitle,
   CardText,
   Button,
+  CardFooter,
 } from '@bootstrap-styled/v4';
 
+import DefaultImg from '../../assets/images/image-not-found.png'
 
 const MyImg = styled.img`
   width: 100%;
   height: 200px;
 `;
-
-//TODO: In server add title et createAt or fake date
 
 const theme = {
   'card-bg': '#313131',
@@ -27,14 +27,17 @@ const theme = {
 const Post = ({ postData }) => {
   console.log('postData', postData)
   return (
-    <Card theme={theme} width="30%" style={{ margin: '15px'}} >
-      <MyImg src="https://zupimages.net/up/20/33/vvju.jpg" alt="Card image cap" />
+    <Card onClick={() => console.log('click', postData.id)} theme={theme} style={{ margin: '15px', width: '30%'}} >
+      <MyImg src={postData.imageUrl ? postData.imageUrl : DefaultImg} alt={postData.title} />
       <CardBlock>
-        <CardTitle>Card title</CardTitle>
-        <CardSubtitle>Card subtitle</CardSubtitle>
-        <CardText>Some quick example text to build on the card title and make up the bulk of the Card content.</CardText>
-        <Button color="primary">Go somewhere</Button>
+        <CardTitle>{postData.title}</CardTitle>
+        <CardSubtitle style={{margin: '5px 0'}}>{`${postData.price}`}</CardSubtitle>
+        <CardSubtitle>{`${postData.size} m²`}</CardSubtitle>
+        <CardText>{postData.description}</CardText>
       </CardBlock>
+      <CardFooter style={{ padding: '10px 20px', fontStyle: 'italic', fontSize: '0.8rem' }}>
+        {`Posted by :   ${postData.autor}`}
+      </CardFooter>
     </Card>
   )
 }
